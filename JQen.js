6565771941331353ed5c00a87ded9b90b20ad3ee
Hani Ammar <hani.ammar@outlook.com>
2025-06-26 15:34:46 +0300

    Update Qlib

--- JQen.js
+++ JQen.js
@@ -14,7 +14,7 @@ var R=a=>{for(;0<a.length;)a.shift()(c)},S=[],T=[],fa=()=>{var a=c.preRun.shift(
 l+=3}var p=l+1;e=l=X(p);k=J;if(0<p){p=e+p-1;for(var G=0;G<g.length;++G){var m=g.codePointAt(G);if(127>=m){if(e>=p)break;k[e++]=m}else if(2047>=m){if(e+1>=p)break;k[e++]=192|m>>6;k[e++]=128|m&63}else if(65535>=m){if(e+2>=p)break;k[e++]=224|m>>12;k[e++]=128|m>>6&63;k[e++]=128|m&63}else{if(e+3>=p)break;k[e++]=240|m>>18;k[e++]=128|m>>12&63;k[e++]=128|m>>6&63;k[e++]=128|m&63;G++}}k[e]=0}}return l},array:g=>{var l=X(g.length);I.set(g,l);return l}};a=c["_"+a];var n=[],H=0;if(f)for(var r=0;r<f.length;r++){var ba=
 h[d[r]];ba?(0===H&&(H=ka()),n[r]=ba(f[r])):n[r]=f[r]}d=a(...n);return d=function(g){0!==H&&la(H);if("string"===b)if(g){for(var l=g+NaN,e=g;!(e>=l)&&J[e];)++e;g=ja.decode(J.subarray(g,e))}else g="";else g="boolean"===b?!!g:g;return g}(d)};c.noExitRuntime&&(U=c.noExitRuntime);c.printErr&&(z=c.printErr);c.wasmBinary&&(A=c.wasmBinary);c.cwrap=(a,b,d,f)=>{var h=!d||d.every(n=>"number"===n||"boolean"===n);return"string"!==b&&h&&!f?c["_"+a]:(...n)=>ma(a,b,d,n)};
 var na,la,X,ka,oa={c:()=>O(""),b:()=>{U=!1;V=0},d:(a,b)=>{W[a]&&(clearTimeout(W[a].id),delete W[a]);if(!b)return 0;var d=setTimeout(()=>{delete W[a];ia(()=>na(a,performance.now()))},b);W[a]={id:d,m:b};return 0},e:a=>{var b=J.length;a>>>=0;if(2147483648<a)return!1;for(var d=1;4>=d;d*=2){var f=b*(1+.2/d);f=Math.min(f,a+100663296);a:{f=(Math.min(2147483648,65536*Math.ceil(Math.max(a,f)/65536))-F.buffer.byteLength+65535)/65536|0;try{F.grow(f);L();var h=1;break a}catch(n){}h=void 0}if(h)return!0}return!1},
-a:ha},Y=await (async function(){function a(d){Y=d.exports;F=Y.f;L();d=Y;c._JQen_Render=d.h;na=d.i;la=d.j;X=d.k;ka=d.l;M--;c.monitorRunDependencies?.(M);0==M&&N&&(d=N,N=null,d());return Y}M++;c.monitorRunDependencies?.(M);var b={a:oa};if(c.instantiateWasm)return new Promise(d=>{c.instantiateWasm(b,(f,h)=>{d(a(f,h))})});P??=c.locateFile?c.locateFile("JQen.wasm",v):v+"JQen.wasm";return a((await ea(b)).instance)}());
+a:ha},Y=await (async function(){function a(d){Y=d.exports;F=Y.f;L();d=Y;c._JQenRender=d.h;na=d.i;la=d.j;X=d.k;ka=d.l;M--;c.monitorRunDependencies?.(M);0==M&&N&&(d=N,N=null,d());return Y}M++;c.monitorRunDependencies?.(M);var b={a:oa};if(c.instantiateWasm)return new Promise(d=>{c.instantiateWasm(b,(f,h)=>{d(a(f,h))})});P??=c.locateFile?c.locateFile("JQen.wasm",v):v+"JQen.wasm";return a((await ea(b)).instance)}());
 function Z(){function a(){c.calledRun=!0;if(!B){K=!0;Y.g();D?.(c);c.onRuntimeInitialized?.();if(c.postRun)for("function"==typeof c.postRun&&(c.postRun=[c.postRun]);c.postRun.length;){var b=c.postRun.shift();S.push(b)}R(S)}}if(0<M)N=Z;else{if(c.preRun)for("function"==typeof c.preRun&&(c.preRun=[c.preRun]);c.preRun.length;)fa();R(T);0<M?N=Z:c.setStatus?(c.setStatus("Running..."),setTimeout(()=>{setTimeout(()=>c.setStatus(""),1);a()},1)):a()}}
 if(c.preInit)for("function"==typeof c.preInit&&(c.preInit=[c.preInit]);0<c.preInit.length;)c.preInit.shift()();Z();K?moduleRtn=c:moduleRtn=new Promise((a,b)=>{D=a;E=b});
 
@@ -30,3 +30,23 @@ if (typeof exports === 'object' && typeof module === 'object') {
   module.exports.default = JQenModule;
 } else if (typeof define === 'function' && define['amd'])
   define([], () => JQenModule);
+const LoadJQenRender = () => {
+    return JQenModule().then((i) => {
+        i.JQenRender = i.cwrap(
+            "JQenRender",
+            "string",
+            ["string", "number", "string", "number", "string", "number"]
+        );
+
+        return (a, b, c = "") => {
+            return i.JQenRender(
+                a,
+                a.length,
+                b,
+                b.length,
+                c,
+                c.length
+            );
+        };
+    });
+};
